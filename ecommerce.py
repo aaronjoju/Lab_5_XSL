529a9219a247a1d320387c6ecf647569e84f8e3e
aaronjoju <aaron.joju@mca.christuniversity.in>
2023-08-28 09:10:37 +0530

    init

--- ecommerce.py
+++ ecommerce.py
@@ -23,9 +23,9 @@ schema = etree.XMLSchema(xsd)
 # Validate transformed HTML against XSD schema
 validation_result = schema.validate(html)
 
-if validation_result:
-    print("Validation successful!")
-else:
-    # print("Validation errors:")
-    print("Validation Result :",validation_result)
-    print(schema.error_log)
+# if validation_result:
+#     print("Validation successful!")
+# else:
+#     # print("Validation errors:")
+#     print("Validation Result :",validation_result)
+#     print(schema.error_log)
